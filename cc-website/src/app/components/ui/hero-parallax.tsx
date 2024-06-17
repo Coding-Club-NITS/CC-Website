2901f0143aa41aa3a0f9d5a057357efdcc2f2e30
Arnab <arnabkumar.coding@gmail.com>
2024-06-17 11:56:06 +0530

    Fix: Components importing

--- cc-website/src/app/components/ui/hero-parallax.tsx
+++ cc-website/src/app/components/ui/hero-parallax.tsx
@@ -10,7 +10,7 @@ import {
 import Image from "next/image";
 import Link from "next/link";
 import { WavyBackground } from "../ui/wave";
-
+import Navbar from "../navbar";
 export const HeroParallax = ({
   products,
 }: {
@@ -58,38 +58,7 @@ export const HeroParallax = ({
   return (
     <>
      {/* Navbar begins here */}
-    <nav className="absolute w-full z-10 pt-5 pr-5">
-  <div className="flex items-center justify-between">
-    <a href="#"
-      className="mr-4 block cursor-pointer py-1.5 font-sans text-base font-semibold leading-relaxed tracking-normal text-inherit antialiased">
-      
-    </a>
-    <div className="hidden lg:block">
-      <ul className="flex flex-col gap-2 my-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
-        <li className="block p-1 font-sans text-sm antialiased font-medium leading-normal text-gray-500">
-          <a href="#" className="flex items-center transition-colors hover:text-red-500">
-            Pages
-          </a>
-        </li>
-        <li className="block p-1 font-sans text-sm antialiased font-medium leading-normal text-gray-500">
-          <a href="#" className="flex items-center transition-colors hover:text-red-500">
-            Account
-          </a>
-        </li>
-        <li className="block p-1 font-sans text-sm antialiased font-medium leading-normal text-gray-500">
-          <a href="#" className="flex items-center transition-colors hover:text-red-500">
-            Blocks
-          </a>
-        </li>
-        <li className="block p-1 font-sans text-sm antialiased font-medium leading-normal text-gray-500">
-          <a href="#" className="flex items-center transition-colors hover:text-red-500">
-            Docs
-          </a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
+      <Navbar/>
 <WavyBackground className="max-w-4xl mx-auto pb-40 mt-5">
       <p className="text-2xl md:text-4xl lg:text-7xl text-white font-bold inter-var text-center">
         Welcome
--- cc-website/src/app/page.tsx
+++ cc-website/src/app/page.tsx
@@ -1,97 +1,22 @@
-import { HeroParallax } from './components/ui/hero-parallax'
+'use client'
+// import { HeroParallax } from './components/ui/hero-parallax'
 import ScrollCards from './components/scrollcards'
 import Footer from './components/footercomp'
-import { InfiniteMovingCards } from "./components/ui/comments";
+import Testimonials from './components/testimonials'
 import { FlipWordsDemo } from './components/flipword';
-const testimonials = [
-  {
-    quote:
-      "It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair.",
-    name: "Siddhid Saha",
-    title: "Moderator Coding Club NITS",
-  },
-  {
-    quote:
-      "To be, or not to be, that is the question: Whether 'tis nobler in the mind to suffer The slings and arrows of outrageous fortune, Or to take Arms against a Sea of troubles, And by opposing end them: to die, to sleep.",
-    name: "William Shakespeare",
-    title: "Hamlet",
-  },
-  {
-    quote: "All that we see or seem is but a dream within a dream.",
-    name: "Edgar Allan Poe",
-    title: "A Dream Within a Dream",
-  },
-  {
-    quote:
-      "It is a truth universally acknowledged, that a single man in possession of a good fortune, must be in want of a wife.",
-    name: "Jane Austen",
-    title: "Pride and Prejudice",
-  },
-  {
-    quote:
-      "Call me Ishmael. Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would sail about a little and see the watery part of the world.",
-    name: "Herman Melville",
-    title: "Moby-Dick",
-  },
-];
+import Parallax from './components/parallax';
 
-export default function Home() {
-  const products = [
-    {
-      title: "Sessions",
-      link:  "https://res.cloudinary.com/dtpmqu6lw/image/upload/fl_preserve_transparency/v1718524959/mo5fk0clewmk3wwtfree.jpg?_s=public-apps",
-      thumbnail: "https://res.cloudinary.com/dtpmqu6lw/image/upload/fl_preserve_transparency/v1718524959/mo5fk0clewmk3wwtfree.jpg?_s=public-apps"
-    },
-    {
-      title: "Code-Intium",
-      link: "https://res.cloudinary.com/dtpmqu6lw/image/upload/fl_preserve_transparency/v1718526037/IMG20231104200715_ydkaee.jpg?_s=public-apps",
-      thumbnail: "https://res.cloudinary.com/dtpmqu6lw/image/upload/fl_preserve_transparency/v1718526037/IMG20231104200715_ydkaee.jpg?_s=public-apps"
-    },
-    {
-      title: "The Family",
-      link: "https://res.cloudinary.com/dtpmqu6lw/image/upload/fl_preserve_transparency/v1718524961/b8ydp5nvc0kryeyl9n8w.jpg?_s=public-apps",
-      thumbnail: "https://res.cloudinary.com/dtpmqu6lw/image/upload/fl_preserve_transparency/v1718524961/b8ydp5nvc0kryeyl9n8w.jpg?_s=public-apps"
-    },
-    {
-      title: "Contests",
-      link: "https://res.cloudinary.com/dtpmqu6lw/image/upload/fl_preserve_transparency/v1718524986/txgmdrlbtvdpmvq7cfng.jpg?_s=public-apps",
-      thumbnail: "https://res.cloudinary.com/dtpmqu6lw/image/upload/fl_preserve_transparency/v1718524986/txgmdrlbtvdpmvq7cfng.jpg?_s=public-apps"
-    },
-    {
-      title: "Sessions",
-      link:  "https://res.cloudinary.com/dtpmqu6lw/image/upload/fl_preserve_transparency/v1718524959/mo5fk0clewmk3wwtfree.jpg?_s=public-apps",
-      thumbnail: "https://res.cloudinary.com/dtpmqu6lw/image/upload/fl_preserve_transparency/v1718524959/mo5fk0clewmk3wwtfree.jpg?_s=public-apps"
-    },
-    {
-      title: "Code-Intium",
-      link: "https://res.cloudinary.com/dtpmqu6lw/image/upload/fl_preserve_transparency/v1718526037/IMG20231104200715_ydkaee.jpg?_s=public-apps",
-      thumbnail: "https://res.cloudinary.com/dtpmqu6lw/image/upload/fl_preserve_transparency/v1718526037/IMG20231104200715_ydkaee.jpg?_s=public-apps"
-    },
-    {
-      title: "The Family",
-      link: "https://res.cloudinary.com/dtpmqu6lw/image/upload/fl_preserve_transparency/v1718524961/b8ydp5nvc0kryeyl9n8w.jpg?_s=public-apps",
-      thumbnail: "https://res.cloudinary.com/dtpmqu6lw/image/upload/fl_preserve_transparency/v1718524961/b8ydp5nvc0kryeyl9n8w.jpg?_s=public-apps"
-    },
-    {
-      title: "Contests",
-      link: "https://res.cloudinary.com/dtpmqu6lw/image/upload/fl_preserve_transparency/v1718524986/txgmdrlbtvdpmvq7cfng.jpg?_s=public-apps",
-      thumbnail: "https://res.cloudinary.com/dtpmqu6lw/image/upload/fl_preserve_transparency/v1718524986/txgmdrlbtvdpmvq7cfng.jpg?_s=public-apps"
-    },
-  ];
-
+export default function page(){
   return (
     <div className='bg-black-900'>
-      <HeroParallax products={products}/>
+      <Parallax/>
       <ScrollCards/>
       <div className="h-[40rem] rounded-md flex flex-col antialiased items-center justify-center relative overflow-hidden">
       <FlipWordsDemo/>
-      <InfiniteMovingCards
-        items={testimonials}
-        direction="right"
-        speed="slow"
-      />
+      <Testimonials/>
       <Footer/>
     </div>
     </div> 
   );
 }
+
