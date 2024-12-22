8091b86fe8be0c4d258b87c780fb69a247d8f443
Arnab <arnabkumar.coding@gmail.com>
2024-12-23 01:42:04 +0530

    Add:Routing

--- next.config.mjs
+++ next.config.mjs
@@ -2,10 +2,17 @@
 
 const nextConfig = {
   images: {
-    domains: ['res.cloudinary.com', "asset.cloudinary.com", "picsum.photos", "media.dev.to", "images.crunchbase.com", "pathrise-website-guide-wp.s3.us-west-1.amazonaws.com",
+    domains: [
+      "res.cloudinary.com",
+      "asset.cloudinary.com",
+      "picsum.photos",
+      "media.dev.to",
+      "images.crunchbase.com",
+      "pathrise-website-guide-wp.s3.us-west-1.amazonaws.com",
+      "images.unsplash.com",
+      "assets.aceternity.com",
     ],
   },
 };
 
 export default nextConfig;
-
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,21 @@
-'use client'
+"use client";
 // import { HeroParallax } from './components/ui/hero-parallax'
-import ScrollCards from './components/scrollcards'
-import Footer from './components/footercomp'
-import Testimonials from './components/testimonials'
-import { FlipWordsDemo } from './components/flipword';
-import Parallax from './components/parallax';
+import ScrollCards from "./components/scrollcards";
+import Footer from "./components/footercomp";
+import Testimonials from "./components/testimonials";
+import { FlipWordsDemo } from "./components/flipword";
+import Parallax from "./components/parallax";
 
-export default function page(){
+export default function page() {
   return (
-    <div className='dark:bg-black-900 bg-white-800'>
-      <Parallax/>
-      <ScrollCards/>
+    <div className="dark:bg-black-900 bg-white-800">
+      <Parallax />
+      <ScrollCards />
       <div className="h-[40rem] rounded-md flex flex-col antialiased items-center justify-center relative overflow-hidden">
-      <FlipWordsDemo/>
-      <Testimonials/>
-      <Footer/>
+        <FlipWordsDemo />
+        <Testimonials />
+        <Footer />
+      </div>
     </div>
-    </div> 
   );
 }
-
