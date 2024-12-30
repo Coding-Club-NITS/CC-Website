0ad74e21f0a3de115b6516e37c47bbfe29eb8e19
Arnab <arnabkumar.coding@gmail.com>
2024-12-30 13:33:41 +0530

    Fix:Teams Page UI

--- src/app/components/focuscard.tsx
+++ src/app/components/focuscard.tsx
@@ -5,11 +5,15 @@ export default function FocusCardsDemo() {
   return (
     <div>
       {Object.entries(team).map(([key, section]) => (
-        <div key={key}>
-          <h1 className="border-y-2 border-orange-300 text-red-500 align-middle text-center text-2xl font-sans tracking-tight md:text-4xl m-5 p-1">
-            {section.title}
-          </h1>
-          <FocusCards cards={section.data} />
+        <div key={key} className="mt-10  backdrop-blur-md">
+          <div className="bg-white/10 backdrop-blur-lg py-4">
+            <h1 className="text-white text-3xl font-bold italic text-center w-full">
+              {section.title}
+            </h1>
+          </div>
+          <div className="w-full bg-blue-gray-900/30 backdrop-blur-md py-4 ">
+            <FocusCards cards={section.data} />
+          </div>
         </div>
       ))}
     </div>
